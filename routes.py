63dac7a5ba0b2088ae1bb3d6784f74d5ecf860a8
siem <siemarell@gmail.com>
2018-05-23 12:39:52 +0300

    log msg

--- routes.py
+++ routes.py
@@ -10,9 +10,17 @@ def msg_handler():
     current_app.logger.info("Request json is: %s" % request.json)
     data = request.json
     address = data['message']['text']
+    if address.startswith('/'):
+        return "OK", 200
     chat_id = data['message']['chat']['id']
     if not waves.validate_address(address):
+        current_app.logger.info(f'Invalid waves testnet address: "{address}"')
         bot.send_message(chat_id, f'Invalid waves testnet address: "{address}"')
     else:
-        waves.transfer_asset(address)
+        if waves.transfer_asset(address):
+            current_app.logger.info(f'I\'ve sent you 0.01 waves to "{address}"')
+            bot.send_message(chat_id, f'I\'ve sent you 0.01 waves to "{address}"')
+        else:
+            current_app.logger.info(f'Failed to send waves to "{address}"')
+            bot.send_message(chat_id, f'Failed to send waves to "{address}"')
     return "OK", 200
