ac8d98a41f5822e34e622ce7ef0043c752f93708
siem <siemarell@gmail.com>
2018-05-23 11:40:09 +0300

    variable rename

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 BOT_TOKEN = "558192444:AAHXehbN2ItrZahpH_mDqig76cvcLspFtVg"
-URL = "https://api.telegram.org/bot%s/" % BOT_TOKEN
+BOT_URL = "https://api.telegram.org/bot%s/" % BOT_TOKEN
+
+API_KEY = "bomba"
+NODE_ADDRESS = "http://127.0.0.1"
--- poller.py
+++ poller.py
@@ -1,22 +1,21 @@
 from threading import Thread
 import requests
-from config import URL
+from config import BOT_URL
 import logging
 import time
 import json
 #import db
 
 
-class Puller(Thread):
+class Poller(Thread):
     def run(self):
         last = 0
         while True:
             #last = db.get_last_update_index()
-            r = requests.get(URL + "getUpdates?offset=%s" % (last + 1))
+            r = requests.get(BOT_URL + "getUpdates?offset=%s" % (last + 1))
             if r.status_code == 200:
                 for message in r.json()["result"]:
                     last = int(message["update_id"])
-                    #db.update_last_update_index(int(message["update_id"]))
                     requests.post("http://localhost:5000/",
                                   data=json.dumps(message),
                                   headers={'Content-type': 'application/json',
